d8a127cab911c82a04174882921334ee847554f5
chenmx00 <chenmx0064@gmail.com>
2020-10-18 23:17:33 -0700

    first commit

---
+++ MXBT/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MXBT
+//
+//  Created by Minxing Chen on 10/18/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

abce99cc1a0715a37a251e67778ca4bfaa346a2e
chenmx00 <chenmx0064@gmail.com>
2020-10-28 12:59:31 -0700

    file write implemented

--- MXBT/dataFetch.cpp
+++ MXBT/dataFetch.cpp
@@ -31,10 +31,14 @@ dataFetch::dataFetch(char *symbol, long start_date, long end_date, char filename
     FILE *fp;
     string yahoo_url = string("https://query1.finance.yahoo.com/v7/finance/download/") + string(symbol) + string("?period1=") + to_string(start_date) + string("&period2=") + to_string(end_date) + string("&interval=") + string(interval) + string("&events=history&crumb=") + get_crumb_and_cookies(symbol, cookie, crumbFilename);
     CURL *curl = curl = curl_easy_init(); // Curl initialization.
-    curl_easy_setopt(curl, CURLOPT_COOKIE, cookie); //Set cookies.
+    curl_easy_setopt(curl, CURLOPT_COOKIEFILE, cookie); //Set cookies.
     curl_easy_setopt(curl, CURLOPT_URL, yahoo_url.c_str());
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write);
-    
-    
-    
+    fp = fopen(filename, "wb");
+    // writing data into the file.
+    if (fp) {
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+        curl_easy_perform(curl);
+        fclose(fp);
+    }
 }

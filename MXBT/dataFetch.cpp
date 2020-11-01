8e11c50070d4cb08fdebaf1e6dce0032695d51ec
“chenmx00” <chenmx0064@gmail.com>
2020-10-31 23:55:56 -0400

    v3

--- MXBT/dataFetch.cpp
+++ MXBT/dataFetch.cpp
@@ -25,7 +25,9 @@ size_t write(void *ptr, size_t size, size_t nmemb, FILE *stream){
     return fwrite(ptr, size, nmemb, stream);
 }
 
-const char* get
+const char* getCrumb(){
+    
+}
 
 dataFetch::dataFetch(char *symbol, long start_date, long end_date, char filename[MAX_LENGTH], char cookie[MAX_LENGTH], char crumbFilename[MAX_LENGTH]){
     auto interval = "one"; // unit: days
